3f77d1d7b7b7aebed27c3fe2e9874af677f7ed52
Ackchyrin <86160552+Ackchyrin@users.noreply.github.com>
2022-03-19 12:10:27 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const routerErrorWay = require('./routes/errorsway');
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+
+const usersRoute = require('./routes/users');
+const cardsRoute = require('./routes/cards');
 
 const app = express();
+
+const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '622458d61f7bc162137cd177', //   _id созданного пользователя
+    _id: '622f8df951e4723e26c99ec4',
   };
 
   next();
 });
-// подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
+
+app.use('/', usersRoute);
+app.use('/', cardsRoute);
+
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'Ошибка 404. Страница не найдена' });
 });
 
-app.use(routerErrorWay);
-
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
